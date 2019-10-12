da643580258649d90369f21541d4ad1b37b2f880
Tyler Crompton <tyler@tylercrompton.com>
2019-10-12 17:05:11 -0500

    added companies table and company model

---
+++ database/migrations/2019_10_12_205942_create_companies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100)->index();
+            $table->string('email', 254)->nullable();
+            $table->string('website')->nullable();
+            $table->string('logo_path')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
