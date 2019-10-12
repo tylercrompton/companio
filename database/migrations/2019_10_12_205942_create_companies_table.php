bdaf2e5daef5d29672ad6b4b893254866195f8be
Tyler Crompton <tyler@tylercrompton.com>
2019-10-12 17:14:14 -0500

    added some whitespace to company table migration code

--- database/migrations/2019_10_12_205942_create_companies_table.php
+++ database/migrations/2019_10_12_205942_create_companies_table.php
@@ -15,6 +15,7 @@ class CreateCompaniesTable extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->bigIncrements('id');
+
             $table->string('name', 100)->index();
             // The maximum length of a valid email address is 254 characters.
             // See https://www.rfc-editor.org/errata_search.php?rfc=3696&eid=1690
@@ -22,6 +23,7 @@ class CreateCompaniesTable extends Migration
             $table->string('email', 254)->nullable();
             $table->string('website')->nullable();
             $table->string('logo_path')->nullable();
+
             $table->timestamps();
             $table->softDeletes();
         });
