49a755691d27a463ddaadc1fe384772274b9b02a
Tyler Crompton <tyler@tylercrompton.com>
2019-10-11 20:45:48 -0500

    added user table seed

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\User::insert([
+            'name' => 'Administrator',
+            'email' => 'admin@site.com',
+            'password' => 'password',
+        ]);
+        App\User::insert([
+            'name' => 'Manager',
+            'email' => 'manager@site.com',
+            'password' => 'password',
+        ]);
+    }
+}
