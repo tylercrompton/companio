fd91c6de02374c27104d51a46ce8c0fb8f44fb50
Tyler Crompton <tyler@tylercrompton.com>
2019-10-15 18:08:15 -0500

    added "add" button to companies.index view

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -1,10 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="d-flex justify-content-between align-items-start">
+        <h1>Companies</h1>
+        <a href="{{ route('companies.create') }}" class="btn btn-primary">Add</a>
+    </div>
+
     @if($companies->isEmpty())
         <p class="container">No companies were found.</p>
     @else
-        <ul class="x-ul grid g-3">
+        <ul class="list-unstyled grid g-3">
             @foreach($companies as $company)
                 <li class="card text-center">
                     <img class="card-img-top company-img" src="/images/logo-default.png" alt="Company logo">
