fbde0190bd818dd7d04a5d0d0db6fac0d4c80ae1
Tyler Crompton <tyler@tylercrompton.com>
2019-10-16 05:21:14 -0500

    created employee.show view

--- resources/views/employee/show.blade.php
+++ resources/views/employee/show.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="d-flex flex-column align-items-center">
+        <h1 class="text-center">{{ $employee->first_name }} {{ $employee->last_name }}</h1>
+        <div>
+            @can('view', $company)
+                <a href="{{ route('companies.show', compact('company'), false) }}">{{ $company->name }}</a>
+            @else
+                <span class="text-muted">{{ $company->name }}</span>
+            @endcan
+        </div>
+        <a href="tel:{{ $employee->phone }}">{{ $employee->phone }}</a>
+        <a href="mailto:{{ $employee->email }}">{{ $employee->email }}</a>
+    </div>
 @endsection
