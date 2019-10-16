c6a132ca391af2deaa0de3fdb549b40f4a944d85
Tyler Crompton <tyler@tylercrompton.com>
2019-10-16 08:14:46 -0500

    created user.show view

--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -1,4 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
+    <div class="d-flex flex-column align-items-center">
+        <h1 class="text-center">{{ $user->name }}</h1>
+        <div>{{ __(ucfirst($user->role)) }}</div>
+        <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
+    </div>
 @endsection
