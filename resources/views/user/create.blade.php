ac8f09430973cde6b0bb61e5cf4a82b4eeeabb1e
Tyler Crompton <tyler@tylercrompton.com>
2019-10-18 05:22:22 -0500

    finished the user-management views

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
+    <h1>{{ __('Add User') }}</h1>
+
+    {!! Form::model(\App\User::make(), ['route' => 'users.store']) !!}
+        <div class="form-group">
+            {!! Form::label('name', __('Name')) !!}
+            {!! Form::text('name', null, ['class' => 'form-control', 'maxlength' => '100', 'placeholder' => __('Name'), 'required' => 'required']) !!}
+        </div>
+
+        <div class="form-group">
+            {{-- The server will completely ignore this field. This is strictly for informational purposes, but the code is here should the functionality be desired later. --}}
+            {!! Form::label('role', __('Role')) !!}
+            {!! Form::select('size', [\App\UserRole::admin => __('Administrator'), \App\UserRole::manager => __('Manager'), \App\UserRole::user => __('User')], \App\UserRole::manager, ['class' => 'form-control', 'disabled' => 'disabled']) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('email', __('Email Address')) !!}
+            {!! Form::email('email', null, ['class' => 'form-control', 'maxlength' => '254', 'placeholder' => __('Email Address'), 'required' => 'required']) !!}
+        </div>
+
+        <p class="form-group">Upon creation, the user will receive an email to set their password.</p>
+
+        {!! Form::submit(__('Save'), ['class' => 'btn btn-primary']) !!}
+    {!! Form::close() !!}
 @endsection
--- resources/views/user/show.blade.php
+++ resources/views/user/show.blade.php
@@ -3,13 +3,48 @@
 @section('content')
     <div class="d-flex flex-column align-items-center">
         <h1 class="text-center">{{ $user->name }}</h1>
+
+        @can('update', $user)
+            <a href="{{ route('users.edit', compact('user'), false) }}" class="btn btn-primary mb-3">{{ __('Edit') }}</a>
+        @endcan
+
         <div>
             {!! \Octicons\Octicon::person() !!}
             {{ __(ucfirst($user->role)) }}
         </div>
+
         <div>
             {!! \Octicons\Octicon::mail() !!}
             <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
         </div>
+
+        @can('delete', $user)
+            {{-- TODO: Fix confirmation dialog --}}
+            {!! Form::model($user, ['class' => 'mt-3', 'id' => 'deleteForm', 'method' => 'delete', 'route' => ['users.destroy', 'user' => $user]]) !!}
+                {!! Form::submit('Delete', ['class' => 'btn btn-outline-danger']) !!}
+            {!! Form::close() !!}
+        @endcan
     </div>
+
+    @can('delete', $user)
+        <div class="modal fade" id="confirmationModal" tabindex="-1" role="dialog" aria-labelledby="confirmationModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <div class="modal-title h5" id="confirmationModalLabel">Confirm Deletion</div>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Cancel">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        Are you sure that you want to delete {{ $user->name }}?
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+                        <button type="button" id="confirmationButton" class="btn btn-danger">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endcan
 @endsection
