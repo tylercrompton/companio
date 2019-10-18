fd7fde3e0f6eaf7592dad7242958ae5177404f82
Tyler Crompton <tyler@tylercrompton.com>
2019-10-18 08:01:41 -0500

    added validation to user forms

--- resources/views/user/create.blade.php
+++ resources/views/user/create.blade.php
@@ -6,18 +6,64 @@
     {!! Form::model(\App\User::make(), ['route' => 'users.store']) !!}
         <div class="form-group">
             {!! Form::label('name', __('Name')) !!}
-            {!! Form::text('name', null, ['class' => 'form-control', 'maxlength' => '100', 'placeholder' => __('Name'), 'required' => 'required']) !!}
+            {!! Form::text(
+                'name',
+                null,
+                [
+                    'class' => 'form-control' . ($errors->any() ? ($errors->has('name') ? ' is-invalid' : ' is-valid') : ''),
+                    'maxlength' => '100',
+                    'placeholder' => __('Name'),
+                    'required' => 'required',
+                ]
+            ) !!}
+            @if($errors->has('name'))
+                <div class="invalid-feedback">{{ implode(' ', $errors->get('name')) }}</div>
+            @else
+                <div class="valid-feedback">Looks good!</div>
+            @endif
         </div>
 
         <div class="form-group">
             {{-- The server will completely ignore this field. This is strictly for informational purposes, but the code is here should the functionality be desired later. --}}
             {!! Form::label('role', __('Role')) !!}
-            {!! Form::select('size', [\App\UserRole::admin => __('Administrator'), \App\UserRole::manager => __('Manager'), \App\UserRole::user => __('User')], \App\UserRole::manager, ['class' => 'form-control', 'disabled' => 'disabled']) !!}
+            {!! Form::select(
+                'role',
+                [
+                    \App\UserRole::admin => __('Administrator'),
+                    \App\UserRole::manager => __('Manager'),
+                    \App\UserRole::user => __('User'),
+                ],
+                \App\UserRole::manager,
+                [
+                    'class' => 'form-control' . ($errors->any() ? ($errors->has('role') ? ' is-invalid' : ' is-valid') : ''),
+                    'disabled' => 'disabled',
+                    'required' => 'required',
+                ]
+            ) !!}
+            @if($errors->has('role'))
+                <div class="invalid-feedback">{{ implode(' ', $errors->get('role')) }}</div>
+            @else
+                <div class="valid-feedback">Looks good!</div>
+            @endif
         </div>
 
         <div class="form-group">
             {!! Form::label('email', __('Email Address')) !!}
-            {!! Form::email('email', null, ['class' => 'form-control', 'maxlength' => '254', 'placeholder' => __('Email Address'), 'required' => 'required']) !!}
+            {!! Form::email(
+                'email',
+                null,
+                [
+                    'class' => 'form-control' . ($errors->any() ? ($errors->has('email') ? ' is-invalid' : ' is-valid') : ''),
+                    'maxlength' => '254',
+                    'placeholder' => __('Email Address'),
+                    'required' => 'required',
+                ]
+            ) !!}
+            @if($errors->has('email'))
+                <div class="invalid-feedback">{{ implode(' ', $errors->get('email')) }}</div>
+            @else
+                <div class="valid-feedback">Looks good!</div>
+            @endif
         </div>
 
         <p class="form-group">Upon creation, the user will receive an email to set their password.</p>
--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -6,18 +6,61 @@
     {!! Form::model($user, ['method' => 'put', 'route' => ['users.update', 'user' => $user]]) !!}
         <div class="form-group">
             {!! Form::label('name', __('Name')) !!}
-            {!! Form::text('name', null, ['class' => 'form-control', 'maxlength' => '100', 'placeholder' => $user->name]) !!}
+            {!! Form::text(
+                'name',
+                null,
+                [
+                    'class' => 'form-control' . ($errors->any() ? ($errors->has('name') ? ' is-invalid' : ' is-valid') : ''),
+                    'maxlength' => '100',
+                    'placeholder' => $user->name,
+                ]
+            ) !!}
+            @if($errors->has('name'))
+                <div class="invalid-feedback">{{ implode(' ', $errors->get('name')) }}</div>
+            @else
+                <div class="valid-feedback">Looks good!</div>
+            @endif
         </div>
 
         <div class="form-group">
             {{-- The server will completely ignore this field. This is strictly for informational purposes, but the code is here should the functionality be desired later. --}}
             {!! Form::label('role', __('Role')) !!}
-            {!! Form::select('size', [\App\UserRole::admin => __('Administrator'), \App\UserRole::manager => __('Manager'), \App\UserRole::user => __('User')], null, ['class' => 'form-control', 'disabled' => 'disabled']) !!}
+            {!! Form::select(
+                'role',
+                [
+                    \App\UserRole::admin => __('Administrator'),
+                    \App\UserRole::manager => __('Manager'),
+                    \App\UserRole::user => __('User'),
+                ],
+                \App\UserRole::manager,
+                [
+                    'class' => 'form-control' . ($errors->any() ? ($errors->has('role') ? ' is-invalid' : ' is-valid') : ''),
+                    'disabled' => 'disabled',
+                ]
+            ) !!}
+            @if($errors->has('role'))
+                <div class="invalid-feedback">{{ implode(' ', $errors->get('role')) }}</div>
+            @else
+                <div class="valid-feedback">Looks good!</div>
+            @endif
         </div>
 
         <div class="form-group">
             {!! Form::label('email', __('Email Address')) !!}
-            {!! Form::email('email', null, ['class' => 'form-control', 'maxlength' => '254', 'placeholder' => $user->email]) !!}
+            {!! Form::email(
+                'email',
+                null,
+                [
+                    'class' => 'form-control' . ($errors->any() ? ($errors->has('email') ? ' is-invalid' : ' is-valid') : ''),
+                    'maxlength' => '254',
+                    'placeholder' => $user->email,
+                ]
+            ) !!}
+            @if($errors->has('email'))
+                <div class="invalid-feedback">{{ implode(' ', $errors->get('email')) }}</div>
+            @else
+                <div class="valid-feedback">Looks good!</div>
+            @endif
         </div>
 
         {!! Form::submit(__('Save'), ['class' => 'btn btn-primary']) !!}
